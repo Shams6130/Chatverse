daedcfd41a75ef91d3e4444ac4194603640691da
shams shahnawaz <shahnawazshams28@gmail.com>
2025-08-08 06:07:02 +0530

    logout-1

--- frontend/src/home/Leftpart/Logout.jsx
+++ frontend/src/home/Leftpart/Logout.jsx
@@ -28,7 +28,7 @@ function Logout() {
   };
   return (
     <div className="h-[10vh] bg-transparent">
-      <form onSubmit={handleLogout}>
+      <form onSubmit={(e) => { e.preventDefault(); handleLogout(); }}>
         <div className="flex space-x-3 items-center ">
           <button>
             <IoMdLogOut className="text-5xl text-white hover:bg-slate-700 duration-300 cursor-pointer rounded-full p-2 ml-2 mt-1" />
